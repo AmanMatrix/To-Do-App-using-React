1d2acf6afa8480f84c91bffd45d63c6c91a47b5d
AmanMatrix <itsamansingla11@gmail.com>
2020-12-13 20:51:31 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from "react"
+import TodoItem from "./Componenets/TodoItem"
+import todosData from "./Componenets/todosData"
+
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            todos: todosData
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+    
+    handleChange(id) {
+        this.setState(prevState => {
+            const updatedTodos = prevState.todos.map(todo => {
+                if (todo.id === id) {
+                    todo.completed = !todo.completed
+                }
+                return todo
+            })
+            return {
+                todos: updatedTodos
+            }
+        })
+    }
+    
+    render() {
+        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>)
+        
+        return (
+            <div className="todo-list">
+                <h1>To-Do Tasks</h1>
+                {todoItems}
+            </div>
+        )    
+    }
+}
+
+export default App
