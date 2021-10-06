f93f995062e6cc1eee428aa5db6f1c34a4cdd988
AmanMatrix <itsamansingla11@gmail.com>
2021-10-06 16:46:06 +0530

    Added new features

--- src/App.js
+++ src/App.js
@@ -1,41 +1,13 @@
-import React from "react"
-import TodoItem from "./Componenets/TodoItem"
-import todosData from "./Componenets/todosData"
-import style from "./index.css"
+import React from 'react';
+import './App.css';
+import TodoList from './components/TodoList';
 
-class App extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            todos: todosData
-        }
-        this.handleChange = this.handleChange.bind(this)
-    }
-    
-    handleChange(id) {
-        this.setState(prevState => {
-            const updatedTodos = prevState.todos.map(todo => {
-                if (todo.id === id) {
-                    todo.completed = !todo.completed
-                }
-                return todo
-            })
-            return {
-                todos: updatedTodos
-            }
-        })
-    }
-    
-    render() {
-        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>)
-        
-        return (
-            <div className="todo-list">
-                <h1>To-Do Tasks</h1>
-                {todoItems}
-            </div>
-        )    
-    }
+function App() {
+  return (
+    <div className='todo-app'>
+      <TodoList />
+    </div>
+  );
 }
 
-export default App
+export default App;
